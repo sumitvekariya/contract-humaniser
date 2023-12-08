ce2d36a73c14704fe707b31219fcf8f879449990
Sarkazein <sarkazein7@gmail.com>
2023-12-08 22:25:12 +0530

    textfield for address & tabs added

--- app/[address]/page.tsx
+++ app/[address]/page.tsx
@@ -2,16 +2,31 @@
 
 import React from 'react'
 import TextField from '@mui/material/TextField';
-import { Box } from '@mui/material';
+import { Box, Tab, Tabs } from '@mui/material';
 import { useParams } from 'next/navigation';
 
 const Page = () => {
+
   const { address: addressParam } = useParams();
   const [address, setAddress] = React.useState(addressParam || '');
+  const [selectedTab, setSelectedTab] = React.useState(0);
+
+
+  /** Custom Hooks */
   return (
-    <Box m={'auto'} width={'full'}>
-      <TextField value={address} onChange={(e) => setAddress(e.target.value)} label="Contract Address*" variant="outlined" />
-    </Box>
+    <div>
+      <Box display={'flex'} width={'full'} mt={3}>
+        <TextField className='w-[50vw] m-auto' value={address} onChange={(e) => setAddress(e.target.value)} label="Contract Address*" variant="outlined" />
+      </Box>
+      <Box mt={3}>
+      <Tabs value={selectedTab} onChange={(e, v) => setSelectedTab(v)} centered>
+        <Tab label="ABI" />
+        <Tab label="Code" />
+        <Tab label="Interact" />
+      </Tabs>
+      </Box>
+
+    </div>
   )
 }
 
