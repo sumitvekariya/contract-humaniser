26ebacbfdd0b0b76dab93e18060eaea168193ed5
Sarkazein <sarkazein7@gmail.com>
2023-12-10 02:22:32 +0530

    typings added

---
+++ lib/model.ts
@@ -0,0 +1,25 @@
+export interface Source {
+  [key: string]: {
+    content: string
+  }
+}
+
+export interface GasFeeEstimate {
+  suggestedMaxPriorityFeePerGas: string;
+  suggestedMaxFeePerGas: string;
+  minWaitTimeEstimate: number;
+  maxWaitTimeEstimate: number;
+}
+
+export nterface GasFeesApiResponse {
+  low: GasFeeEstimate;
+  medium: GasFeeEstimate;
+  high: GasFeeEstimate;
+  estimatedBaseFee: string;
+  networkCongestion: number;
+  latestPriorityFeeRange: string[];
+  historicalPriorityFeeRange: string[];
+  historicalBaseFeeRange: string[];
+  priorityFeeTrend: 'up' | 'down';
+  baseFeeTrend: 'up' | 'down';
+}
