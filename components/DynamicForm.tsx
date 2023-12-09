22a28586c42a98c17a510ec28405747823aab482
Sarkazein <sarkazein7@gmail.com>
2023-12-10 02:22:45 +0530

    Other UI elements implemented

---
+++ components/DynamicForm.tsx
@@ -0,0 +1,170 @@
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { createPublicClient, createWalletClient, custom } from "viem";
+import { useAccount, useChainId } from "wagmi";
+import { Input } from "./ui/input";
+
+interface InputComponent {
+  internalType: string;
+  name: string;
+  type: string;
+  components?: InputComponent[];
+}
+
+interface InputField {
+  components: InputComponent[];
+  internalType: string;
+  name: string;
+  type: string;
+}
+
+interface ContractFunction {
+  inputs: InputField[];
+  name: string;
+  outputs: any[];
+  stateMutability: string;
+  type: string;
+}
+
+interface ContractData {
+  abi: any[],
+  address: string
+}
+
+export const DynamicForm = ({ contractFunction, contractData }: { contractFunction: ContractFunction, contractData: ContractData }) => {
+  // Initialize state for the form
+  const [formState, setFormState] = useState<any>({});
+  const [loading, setLoading] = useState(false);
+  const [res, setRes] = useState<any>(null);
+  const chainId = useChainId();
+  const { address } = useAccount();
+
+  const walletClient = createWalletClient({
+    transport: custom(window.ethereum)
+  })
+  const publicClient = createPublicClient({
+    transport: custom(window.ethereum)
+  })
+
+  const handleInputChange = (path: string, value: string) => {
+    setFormState((prevState: any) => {
+      const newState = { ...prevState };
+      newState[path] = value;
+      return newState;
+    });
+  };
+
+  const renderInputField = (input: InputComponent, path: string) => {
+    const fieldPath = `${path}.${input.name}`;
+
+    // Handle the change event for the input
+    const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      handleInputChange(fieldPath, e.target.value);
+    };
+
+    switch (input.type) {
+      case 'address':
+      case 'uint256':
+      case 'bytes':
+        return (
+          <Input
+            type="text"
+            value={formState[fieldPath] || ''}
+            placeholder={`Enter ${input.type}`}
+            onChange={onChange}
+          />
+        );
+      case 'address[]':
+      case 'uint256[]':
+        return (
+          <Input
+            type="text"
+            value={formState[fieldPath] || ''}
+            placeholder={`Enter ${input.type} separated by commas`}
+            onChange={onChange}
+          />
+        );
+      case 'tuple':
+        return (
+          <div>
+            {input.components!.map((component) => (
+              <div key={component.name}>
+                <label>{component.name}</label>
+                {renderInputField(component, fieldPath)}
+              </div>
+            ))}
+          </div>
+        );
+      default:
+        return (
+          <Input
+            type="text"
+            value={formState[fieldPath] || ''}
+            placeholder={`Enter ${input.type}`}
+            onChange={onChange}
+          />
+        );
+    }
+  };
+
+  const renderForm = () => {
+    return contractFunction.inputs?.map((input) => (
+      <div key={input?.name} className='flex flex-col gap-4 mb-4'>
+        <label>{input?.name}</label>
+        {renderInputField(input, input.name)}
+      </div>
+    ));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formState);
+    // Submit logic here...
+    if (contractFunction.stateMutability === 'view') {
+      publicClient.readContract({
+        abi: contractData.abi,
+        functionName: contractFunction.name,
+        address: contractData.address,
+        args: Object.keys(formState).map(key => formState[key]),
+        account: address,
+        chain: {
+          id: chainId
+        }
+      }).then(async (res: any) => {
+        setRes(res);
+        console.log(res);
+      }).catch((err) => {
+        console.log(err);
+      })
+    } else if (contractFunction.stateMutability === 'nonpayable') {
+      walletClient.writeContract({
+        abi: contractData.abi,
+        functionName: contractFunction.name,
+        address: contractData.address,
+        args: Object.keys(formState).map(key => formState[key]),
+        account: address,
+        chain: {
+          id: chainId
+        }
+      }).then(async (res: any) => {
+        const receipt = await publicClient.waitForTransactionReceipt({ hash: res.hash })
+        console.log(res);
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+  };
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      {renderForm()}
+      <Button style={{ marginTop: '0px', marginLeft: 'auto' }} type="submit" variant='contained'>Submit</Button>
+      { res && (
+        <div className='mt-3'>
+          <label>Output</label>
+          <pre>{res?.toString()}</pre>
+        </div>
+      )}
+    </form>
+  );
+};
---
+++ components/Interact.tsx
@@ -0,0 +1,157 @@
+import React, { useEffect, useState } from 'react'
+import { Input } from './ui/input';
+import { Accordion, AccordionDetails, AccordionSummary, CircularProgress, IconButton, Typography } from '@mui/material';
+import { ChevronUpIcon, ChevronDownIcon } from '@radix-ui/react-icons';
+import { useAccount, useChainId, useContractWrite, usePrepareContractWrite, useWalletClient } from 'wagmi';
+import { createPublicClient, createWalletClient, custom } from 'viem';
+import { DynamicForm } from './DynamicForm';
+import { RiGasStationLine } from "react-icons/ri";
+import { FaGasPump } from "react-icons/fa6";
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { GasFeeEstimate } from '@/lib/model';
+import { Button } from './ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+interface InteractABIProps {
+  abi: [],
+  address: string
+}
+
+const GasCard = () => {
+  const chainId = useChainId();
+  const [loading, setLoading] = useState(false);
+  const [gasDetails, setGasDetails] = useState<GasFeeEstimate | null>(null)
+  useEffect(() => {
+    const fetchGasDetails = async () => {
+      if (!chainId) return
+      setLoading(true);
+      await fetch('/fetchGasDetails', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ chainId })
+      }).then(async (res) => {
+        const data = await res.json();
+        console.log(data);
+        setGasDetails(data);
+        return data
+      })
+        .finally(() => {
+          setLoading(false);
+        })
+    }
+    fetchGasDetails()
+  }, [chainId])
+
+  const renderGasFeeEstimateCard = (
+    estimate: GasFeeEstimate,
+    title: string
+  ) => (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Max Priority Fee: {estimate.suggestedMaxPriorityFeePerGas}</p>
+        <p>Max Fee Per Gas: {estimate.suggestedMaxFeePerGas}</p>
+        <p>Min Wait Time: {estimate.minWaitTimeEstimate} seconds</p>
+        <p>Max Wait Time: {estimate.maxWaitTimeEstimate} seconds</p>
+      </CardContent>
+    </Card>
+  );
+
+  return (
+    <div>
+      {!loading && <div className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {gasDetails?.low &&
+            renderGasFeeEstimateCard(gasDetails.low, "Low Gas Fee Estimate")}
+          {gasDetails?.medium &&
+            renderGasFeeEstimateCard(gasDetails.medium, "Medium Gas Fee Estimate")}
+          {gasDetails?.high &&
+            renderGasFeeEstimateCard(gasDetails.high, "High Gas Fee Estimate")}
+        </div>
+        <Card>
+          <CardContent>
+            <h2 className="font-semibold">Estimated Base Fee</h2>
+            <p>Gas Price: {gasDetails?.estimatedBaseFee}</p>
+            <h2 className="font-semibold">Network Congestion</h2>
+            <p>{gasDetails?.networkCongestion}</p>
+            <h2 className="font-semibold">Latest Priority Fee Range</h2>
+            <p>{gasDetails?.latestPriorityFeeRange?.join(" - ")}</p>
+            <h2 className="font-semibold">Historical Priority Fee Range</h2>
+            <p>{gasDetails?.historicalPriorityFeeRange?.join(" - ")}</p>
+            <h2 className="font-semibold">Historical Base Fee Range</h2>
+            <p>{gasDetails?.historicalBaseFeeRange?.join(" - ")}</p>
+            <h2 className="font-semibold">Priority Fee Trend</h2>
+            <p>{gasDetails?.priorityFeeTrend}</p>
+            <h2 className="font-semibold">Base Fee Trend</h2>
+            <p>{gasDetails?.baseFeeTrend}</p>
+          </CardContent>
+        </Card>
+      </div>}
+      {loading && <div className='flex justify-center m-4'>
+        <CircularProgress color={'primary'} />
+      </div>}
+
+    </div>
+
+  );
+};
+
+const Interact = ({ abi, address }: InteractABIProps) => {
+  console.log(abi, address)
+  return (
+    <div className='w-[60%] m-auto'>
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <Button variant='outline' className='flex gap-2 mb-3'><span>Estimate</span><FaGasPump /></Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent className='w-[70vw] h-[auto]'>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Metamask Gas API</AlertDialogTitle>
+            <AlertDialogDescription>
+              <GasCard />
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            {/* <AlertDialogCancel>Cancel</AlertDialogCancel> */}
+            <AlertDialogAction>Thanks</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      {abi.filter(f => f?.type === 'function')?.map((a: any, i) => (
+        <Accordion key={`${a.name}_${i}`}>
+          <AccordionSummary
+            expandIcon={<ChevronDownIcon />}
+            aria-controls="panel1a-content"
+          >
+            {/* <Typography> */}
+              <pre>{a.name}{`()`}{a.stateMutability === 'view' ? `:${a.outputs.map(o => o.type)}` : ''}</pre>
+            {/* </Typography> */}
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <div className='flex flex-col gap-2 overflow-x-auto'>
+                <DynamicForm contractFunction={a} contractData={{ abi, address }} />
+              </div>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  )
+}
+
+export default Interact
